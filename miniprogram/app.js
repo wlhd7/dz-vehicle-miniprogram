04a8e258a27488c022a7d1869451ed72276b75be
wlhd7 <3509896163@qq.com>
2026-02-23 13:37:06 +0800

    chore: update miniprogram configuration
    - Set actual WeChat cloud environment ID
    - Update appid for production

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,7 +4,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
-        env: 'cloud1', // 替换为你的云开发环境 ID
+        env: 'cloud1-4gfqb62kd1c4383d', // 替换为你的云开发环境 ID
         traceUser: true,
       });
     }
